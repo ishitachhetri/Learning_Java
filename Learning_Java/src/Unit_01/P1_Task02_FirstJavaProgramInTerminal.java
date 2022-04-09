efaa48e09ab04cef68f98cd1d9917c4fd352be31
HP <HP@LAPTOP-Q9OPEMMS.mshome.net>
2022-04-09 15:32:26 +0530

    P1 Task02 Commit!

--- Learning_Java/src/Unit_01/P1_Task02_FirstJavaProgramInTerminal.java
+++ Learning_Java/src/Unit_01/P1_Task02_FirstJavaProgramInTerminal.java
@@ -2,4 +2,9 @@ package Unit_01;
 
 public class P1_Task02_FirstJavaProgramInTerminal {
 
+	public static void main(String[] args) {
+		
+		System.out.println("Hello World. I am a terminal.");
+	}
+
 }
